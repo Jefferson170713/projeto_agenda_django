0797cd258b2a66037fca760426ce66eda4fb837b
Jefferson de Almeida <jefferson.almeida05@aluno.ifce.edu.br>
2025-02-09 11:49:22 -0300

    Adicionando a tabela ao meu painel administrativo do Django

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+# Primeiro vamos importar o modelo Contact
+from contact import models
+
 # Register your models here.
+
+# Agora vamos registrar o modelo Contact no admin e isso aparecerá no painel de administração
+# vamos colocar um decorador para registrar o modelo Contact
+@admin.register( models.Contact)
+class ContactAdmin( admin.ModelAdmin ):
+    # No painel adm, essa função que mostra uma lista mas pode ser uma tupla, mostra em ordem e com o nomes passados
+    list_display = ['id','first_name', 'last_name', 'email', 'phone']
+    # O "-" é um sinal de ordenação
+    ordering = ['-id']
+    # Aqui mostra um campo de pesquisa e por quais itens podemos pesquisar.
+    search_fields = ['id','first_name', 'last_name', 'email']
+    #list_filter = ['created_date']
--- contact/models.py
+++ contact/models.py
@@ -13,6 +13,12 @@ class Contact(models.Model):
     first_name = models.CharField( max_length = 50 )
     last_name = models.CharField( max_length = 50, blank = True)
     phone = models.CharField( max_length = 50)
-    email = models.CharField( max_length = 254, blank = True)
-    created_date = models.DateTimeField( timezone.now )
-    description = models.TextField()
+    email = models.EmailField( max_length = 254, blank = True)
+    created_date = models.DateTimeField( default = timezone.now )
+    description = models.TextField( blank = True)
+
+    # Aqui vamos definir os campos que queremos que apareçam no painel de administração
+    # Este é o campo que será exibido na lista de contatos, mas podemos mudar o que aparece no painel de administração
+    def __str__( self ) -> str:
+        #return super().__str__( f'{self.first_name} {self.last_name}' )
+        return f'{self.first_name} {self.last_name} - {self.email}'
